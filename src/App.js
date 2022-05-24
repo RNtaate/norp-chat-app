b05fe38b43940681b36c98571c1b213a25b67cad
RNtaate <royhyde14@gmail.com>
2022-05-25 01:39:20 +0300

    Add a header in the component design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import { io } from "socket.io-client";
 import Chat from "./components/chat";
+import { useState } from "react"
 
 const socket = io("http://localhost:3001");
 
 function App() {
+  const [currentUsername, setCurrentUsername] = useState("");
 
   return (
     <main className="App d-flex flex-column align-items-center justify-content-center p-2">
       <section className='chat-app-wrapper-section d-flex flex-column align-items-center justify-content-center w-100 overflow-hidden'>
-        <h1 className="chat-heading fs-4 bg-dark w-100 text-light p-3">NORP CHAT APP</h1>
-        <Chat socket={socket} />
+
+        <header className='bg-secondary w-100 d-flex justify-content-between align-items-center px-2'>
+          <h1 className="chat-heading text-light py-2 m-0">NORP CHAT APP</h1>
+          {currentUsername && 
+            <span className='text-light d-flex align-items-center'>
+              <small className='me-2'>{currentUsername}</small>
+              <div className='live-div'></div>
+            </span>
+          }
+        </header>
+
+        <Chat socket={socket} setCurrentUsername={setCurrentUsername} />
       </section>
     </main>
   );
