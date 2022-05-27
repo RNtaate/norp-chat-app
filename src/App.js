40bfb08c9b1a832b5ab31e72c3e6e3e1bb16e863
RNtaate <royhyde14@gmail.com>
2022-05-27 15:58:28 +0300

    Add the global states to it from the Chat.js component
    Update styling of various components using bootstrap classess.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import { io } from "socket.io-client";
 import Chat from "./components/chat";
+import ChatForm from './components/chatForm';
 import { useState } from "react"
 
-const socket = io("http://localhost:3001");
+const socket = io("http://192.168.1.191:3001");
 
 function App() {
   const [currentUsername, setCurrentUsername] = useState("");
+  const [messageList, setMessageList] = useState([])
+  const [userDetails, setUserDetails] = useState({
+    username: null,
+    room: null
+  })
+  const [showMessagesDiv, setShowMessagesDiv] = useState(false);
 
   return (
-    <main className="App d-flex flex-column align-items-center justify-content-center p-2">
+    <main className="App d-flex flex-column align-items-center justify-content-center position-relative">
       <section className='chat-app-wrapper-section d-flex flex-column align-items-center justify-content-center w-100 overflow-hidden'>
 
-        <header className='bg-secondary w-100 d-flex justify-content-between align-items-center px-2'>
+        <header className='bg-secondary w-100 d-flex justify-content-between align-items-center px-2 position-fixed top-0 w-100'>
           <h1 className="chat-heading text-light py-2 m-0">Norp~Chat~App</h1>
-          {currentUsername && 
+          {currentUsername &&
             <span className='text-light d-flex align-items-center'>
               <small className='me-2'>{currentUsername}</small>
               <div className='live-div'></div>
@@ -22,7 +29,25 @@ function App() {
           }
         </header>
 
-        <Chat socket={socket} setCurrentUsername={setCurrentUsername} />
+        <Chat
+          socket={socket}
+          setCurrentUsername={setCurrentUsername}
+          messageList={messageList}
+          setMessageList={setMessageList}
+          userDetails={userDetails}
+          setUserDetails={setUserDetails}
+          showMessagesDiv={showMessagesDiv}
+          setShowMessagesDiv={setShowMessagesDiv}
+        />
+        {showMessagesDiv &&
+          <ChatForm
+            socket={socket}
+            messageList={messageList}
+            setMessageList={setMessageList}
+            userDetails={userDetails}
+            setUserDetails={setUserDetails}
+          />
+        }
       </section>
     </main>
   );
