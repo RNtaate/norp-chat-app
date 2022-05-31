b5cdbc2a7fd9845cac4d1aa480fa34d8cd763357
RNtaate <royhyde14@gmail.com>
2022-05-31 12:22:48 +0300

    Add Modal for rooms using react bootstrap
    Update design for the header by adding a navbar
    
    Create a state and functions to enable displaying and removing of modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { io } from "socket.io-client";
 import Chat from "./components/chat";
 import ChatForm from './components/chatForm';
 import { useState } from "react"
+import { Modal, Button } from "react-bootstrap/"
+import {CHATROOMS} from "./HelperMethods";
 
 const socket = io("http://localhost:3001");
 
@@ -14,17 +16,40 @@ function App() {
     room: null
   })
   const [showMessagesDiv, setShowMessagesDiv] = useState(false);
+  const [show, setShow] = useState(false)
+
+  const [messageObject, setMessageObject] = useState({})
+  const [currentRoom, setCurrentRoom] = useState("");
+
+  const handleClose = () => {
+    setShow(false);
+  }
+
+  const handleShow = () => {
+    setShow(true);
+  }
+
+  const handleSettingRoom = (e) => {
+    setCurrentRoom(e.target.getAttribute('name'));
+    handleClose();
+  }
 
   return (
     <main className="App d-flex flex-column align-items-center justify-content-center position-relative">
       <section className='chat-app-wrapper-section d-flex flex-column align-items-center justify-content-center w-100 overflow-hidden'>
 
-        <header className='bg-secondary w-100 d-flex justify-content-between align-items-center px-2 position-fixed top-0 w-100'>
-          <h1 className="chat-heading text-light py-2 m-0">Norp~Chat~App</h1>
+        <header className='bg-secondary w-100 d-flex flex-column justify-content-between position-fixed top-0'>
+          <nav className='d-flex justify-content-between align-items-center w-100 px-2'>
+            <h1 className="chat-heading text-light py-2 m-0">Norp~Chat~App</h1>
+            {currentUsername &&
+            <Button variant="dark" className='btn-sm' onClick={handleShow}>
+              Rooms
+            </Button>}
+          </nav>
           {currentUsername &&
-            <span className='text-light d-flex align-items-center'>
+            <span className='text-dark px-2 d-flex bg-light align-items-center'>
+              <div className='live-div me-2'></div>
               <small className='me-2'>{currentUsername}</small>
-              <div className='live-div'></div>
             </span>
           }
         </header>
@@ -38,6 +63,10 @@ function App() {
           setUserDetails={setUserDetails}
           showMessagesDiv={showMessagesDiv}
           setShowMessagesDiv={setShowMessagesDiv}
+          messageObject={messageObject}
+          setMessageObject={setMessageObject}
+          currentRoom={currentRoom}
+          setCurrentRoom={setCurrentRoom}
         />
         {showMessagesDiv &&
           <ChatForm
@@ -49,6 +78,26 @@ function App() {
           />
         }
       </section>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Chat Rooms</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul className="room-list-ul">
+            {CHATROOMS.map((room, index) => {
+              return (
+                <li key={index} className="room-list-item" name={room} onClick={handleSettingRoom}>{room}</li>
+              )
+            })}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" className='btn-sm' onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </main>
   );
 }
