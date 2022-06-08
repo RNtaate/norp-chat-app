ae6d8d82acc57095dfd1aa613a41256d0e196565
RNtaate <royhyde14@gmail.com>
2022-06-08 18:08:56 +0300

    Update the nofication icons to respond to changes in messages notifications, even private messages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 
-const NavBar = ({ currentUsername, handleShow, notificationMessages }) => {
+const NavBar = ({
+  currentUsername,
+  handleShow,
+  notificationMessages,
+  privateNotificationMessages }) => {
   return (
     <header className='bg-secondary w-100 d-flex flex-column justify-content-between position-fixed top-0'>
       <nav className='d-flex justify-content-between align-items-center w-100 px-2 py-1'>
         <h1 className="chat-heading text-light py-2 m-0">Norp~Chat~App</h1>
         {currentUsername &&
           <div className="nav-icon-div">
-            <span className='me-3' data-bs-toggle="tooltip" title='Chat Rooms'><i className="fa fa-users" aria-hidden="true" name="group" onClick={handleShow}></i></span>
+            <span className='me-3 position-relative' data-bs-toggle="tooltip" title='Chat Rooms' onClick={handleShow}>
+              <i className="fa fa-users" aria-hidden="true" name="group"></i>
+              {notificationMessages.length > 0 && <span className="position-absolute top-0 start-0 translate-middle bg-danger notification-number-div d-flex justify-content-center align-items-center" style={{ fontSize: "10px", width: "25px", height: "25px", borderRadius: "50%", pointerEvents: "none" }}><b>{notificationMessages.length}</b></span>}
+            </span>
 
-            <span className='position-relative' data-bs-toggle="tooltip" title='Notifications' onClick={handleShow}>
-              <i className="fa fa-envelope" aria-hidden="true" name="private"></i>
-              {notificationMessages.length > 0 && <span className="position-absolute top-0 start-0 translate-middle bg-danger notification-number-div d-flex justify-content-center align-items-center" style={{fontSize: "10px", width: "25px", height: "25px", borderRadius: "50%"}}><b>{notificationMessages.length}</b></span>}
+            <span className='position-relative' data-bs-toggle="tooltip" title='Notifications'>
+              <i className="fa fa-envelope" aria-hidden="true" name="private" onClick={handleShow}></i>
+              {privateNotificationMessages.length > 0 && <span className="position-absolute top-0 start-0 translate-middle bg-danger notification-number-div d-flex justify-content-center align-items-center" style={{ fontSize: "10px", width: "25px", height: "25px", borderRadius: "50%", pointerEvents: "none" }}><b>{privateNotificationMessages.length}</b></span>}
             </span>
           </div>}
       </nav>
