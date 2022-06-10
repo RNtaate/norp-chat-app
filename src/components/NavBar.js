42cf179a68600a8cc99c8b80e15ccc9d491954d5
RNtaate <royhyde14@gmail.com>
2022-06-10 16:03:55 +0300

    Update the class List of notification number div and span
    Remove inline styling from the navbar notification number span and div

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,12 +17,12 @@ const NavBar = ({
           <div className="nav-icon-div">
             <span className='me-4 position-relative' data-bs-toggle="tooltip" title='Chat Rooms' onClick={handleShow}>
               <i className="fa fa-users" aria-hidden="true" name="group"></i>
-              {notificationMessages.length > 0 && <span className="position-absolute top-0 start-0 translate-middle bg-danger notification-number-div d-flex justify-content-center align-items-center" style={{ fontSize: "10px", width: "25px", height: "25px", borderRadius: "50%", pointerEvents: "none" }}><b>{notificationMessages.length}</b></span>}
+              {notificationMessages.length > 0 && <span className="position-absolute top-0 start-0 bg-danger notification-number-rooms-div d-flex justify-content-center align-items-center"><b>{notificationMessages.length}</b></span>}
             </span>
 
             <span className='position-relative' data-bs-toggle="tooltip" title='Notifications'>
               <i className="fa fa-envelope" aria-hidden="true" name="private" onClick={handleShow}></i>
-              {privateNotificationMessages.length > 0 && <span className="position-absolute top-0 start-0 translate-middle bg-danger notification-number-div d-flex justify-content-center align-items-center" style={{ fontSize: "10px", width: "25px", height: "25px", borderRadius: "50%", pointerEvents: "none" }}><b>{privateNotificationMessages.length}</b></span>}
+              {privateNotificationMessages.length > 0 && <span className="position-absolute top-0 start-0 bg-danger notification-number-div d-flex justify-content-center align-items-center"><b>{privateNotificationMessages.length}</b></span>}
             </span>
           </div>}
       </nav>
